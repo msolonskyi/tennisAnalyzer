a91758b16bdf63eb41ffaa8fdc5385fe7e08287b
msolonskyi <nick.solonsky@gmail.com>
2019-05-20 18:01:01 +0200

    countries dictionary has been added

---
+++ db/schemas/tennisAnalyzer/content/countries.sql
@@ -0,0 +1,503 @@
+insert into COUNTRIES (code, name)
+values ('AFG', 'Afghanistan');
+insert into COUNTRIES (code, name)
+values ('ALB', 'Albania');
+insert into COUNTRIES (code, name)
+values ('ALG', 'Algeria');
+insert into COUNTRIES (code, name)
+values ('ASA', 'American Samoa');
+insert into COUNTRIES (code, name)
+values ('AND', 'Andorra');
+insert into COUNTRIES (code, name)
+values ('ANG', 'Angola');
+insert into COUNTRIES (code, name)
+values ('ANT', 'Antigua and Barbuda');
+insert into COUNTRIES (code, name)
+values ('ARG', 'Argentina');
+insert into COUNTRIES (code, name)
+values ('ARM', 'Armenia');
+insert into COUNTRIES (code, name)
+values ('ARU', 'Aruba');
+insert into COUNTRIES (code, name)
+values ('IOC', 'Athletes from Kuwait');
+insert into COUNTRIES (code, name)
+values ('ANZ', 'Australasia');
+insert into COUNTRIES (code, name)
+values ('AUS', 'Australia');
+insert into COUNTRIES (code, name)
+values ('AUT', 'Austria');
+insert into COUNTRIES (code, name)
+values ('AZE', 'Azerbaijan');
+insert into COUNTRIES (code, name)
+values ('BAH', 'Bahamas');
+insert into COUNTRIES (code, name)
+values ('BRN', 'Bahrain');
+insert into COUNTRIES (code, name)
+values ('BAN', 'Bangladesh');
+insert into COUNTRIES (code, name)
+values ('BAR', 'Barbados');
+insert into COUNTRIES (code, name)
+values ('BLR', 'Belarus');
+insert into COUNTRIES (code, name)
+values ('BEL', 'Belgium');
+insert into COUNTRIES (code, name)
+values ('BIZ', 'Belize');
+insert into COUNTRIES (code, name)
+values ('BEN', 'Benin');
+insert into COUNTRIES (code, name)
+values ('BER', 'Bermuda');
+insert into COUNTRIES (code, name)
+values ('BHU', 'Bhutan');
+insert into COUNTRIES (code, name)
+values ('BOH', 'Bohemia');
+insert into COUNTRIES (code, name)
+values ('BOL', 'Bolivia');
+insert into COUNTRIES (code, name)
+values ('BIH', 'Bosnia and Herzegovina');
+insert into COUNTRIES (code, name)
+values ('BOT', 'Botswana');
+insert into COUNTRIES (code, name)
+values ('BRA', 'Brazil');
+insert into COUNTRIES (code, name)
+values ('GUI', 'British Guiana');
+insert into COUNTRIES (code, name)
+values ('HBR', 'British Honduras');
+insert into COUNTRIES (code, name)
+values ('IVB', 'British Virgin Islands');
+insert into COUNTRIES (code, name)
+values ('BWI', 'British West Indies');
+insert into COUNTRIES (code, name)
+values ('BRU', 'Brunei');
+insert into COUNTRIES (code, name)
+values ('BUL', 'Bulgaria');
+insert into COUNTRIES (code, name)
+values ('BUR', 'Burkina Faso');
+insert into COUNTRIES (code, name)
+values ('BIR', 'Burma');
+insert into COUNTRIES (code, name)
+values ('BDI', 'Burundi');
+insert into COUNTRIES (code, name)
+values ('CAM', 'Cambodia');
+insert into COUNTRIES (code, name)
+values ('CMR', 'Cameroon');
+insert into COUNTRIES (code, name)
+values ('CAN', 'Canada');
+insert into COUNTRIES (code, name)
+values ('CPV', 'Cape Verde');
+insert into COUNTRIES (code, name)
+values ('CAY', 'Cayman Islands');
+insert into COUNTRIES (code, name)
+values ('CAF', 'Central African Republic');
+insert into COUNTRIES (code, name)
+values ('CEY', 'Ceylon');
+insert into COUNTRIES (code, name)
+values ('CHA', 'Chad');
+insert into COUNTRIES (code, name)
+values ('CHI', 'Chile');
+insert into COUNTRIES (code, name)
+values ('CHN', 'China');
+insert into COUNTRIES (code, name)
+values ('TPE', 'Chinese Taipei[5]');
+insert into COUNTRIES (code, name)
+values ('COL', 'Colombia');
+insert into COUNTRIES (code, name)
+values ('COM', 'Comoros');
+insert into COUNTRIES (code, name)
+values ('COK', 'Cook Islands');
+insert into COUNTRIES (code, name)
+values ('CRC', 'Costa Rica');
+insert into COUNTRIES (code, name)
+values ('CRO', 'Croatia');
+insert into COUNTRIES (code, name)
+values ('CUB', 'Cuba');
+insert into COUNTRIES (code, name)
+values ('CYP', 'Cyprus');
+insert into COUNTRIES (code, name)
+values ('CZE', 'Czech Republic');
+insert into COUNTRIES (code, name)
+values ('TCH', 'Czechoslovakia');
+insert into COUNTRIES (code, name)
+values ('DAH', 'Dahomey');
+insert into COUNTRIES (code, name)
+values ('COD', 'Democratic Republic of the Congo');
+insert into COUNTRIES (code, name)
+values ('DEN', 'Denmark');
+insert into COUNTRIES (code, name)
+values ('DJI', 'Djibouti');
+insert into COUNTRIES (code, name)
+values ('DMA', 'Dominica');
+insert into COUNTRIES (code, name)
+values ('DOM', 'Dominican Republic');
+insert into COUNTRIES (code, name)
+values ('IHO', 'Dutch East Indies');
+insert into COUNTRIES (code, name)
+values ('GDR', 'East Germany');
+insert into COUNTRIES (code, name)
+values ('TLS', 'East Timor');
+insert into COUNTRIES (code, name)
+values ('ECU', 'Ecuador');
+insert into COUNTRIES (code, name)
+values ('EGY', 'Egypt');
+insert into COUNTRIES (code, name)
+values ('ESA', 'El Salvador');
+insert into COUNTRIES (code, name)
+values ('GEQ', 'Equatorial Guinea');
+insert into COUNTRIES (code, name)
+values ('ERI', 'Eritrea');
+insert into COUNTRIES (code, name)
+values ('EST', 'Estonia');
+insert into COUNTRIES (code, name)
+values ('ETH', 'Ethiopia');
+insert into COUNTRIES (code, name)
+values ('FRO', 'Faroe Islands');
+insert into COUNTRIES (code, name)
+values ('FSM', 'Federated States of Micronesia');
+insert into COUNTRIES (code, name)
+values ('FIJ', 'Fiji');
+insert into COUNTRIES (code, name)
+values ('FIN', 'Finland');
+insert into COUNTRIES (code, name)
+values ('FRA', 'France');
+insert into COUNTRIES (code, name)
+values ('GAB', 'Gabon');
+insert into COUNTRIES (code, name)
+values ('GEO', 'Georgia');
+insert into COUNTRIES (code, name)
+values ('GER', 'Germany');
+insert into COUNTRIES (code, name)
+values ('GHA', 'Ghana');
+insert into COUNTRIES (code, name)
+values ('GBR', 'Great Britain');
+insert into COUNTRIES (code, name)
+values ('GRE', 'Greece');
+insert into COUNTRIES (code, name)
+values ('GRN', 'Grenada');
+insert into COUNTRIES (code, name)
+values ('GUM', 'Guam');
+insert into COUNTRIES (code, name)
+values ('GUA', 'Guatemala');
+insert into COUNTRIES (code, name)
+values ('GBS', 'Guinea-Bissau');
+insert into COUNTRIES (code, name)
+values ('GUY', 'Guyana');
+insert into COUNTRIES (code, name)
+values ('HAI', 'Haiti');
+insert into COUNTRIES (code, name)
+values ('HON', 'Honduras');
+insert into COUNTRIES (code, name)
+values ('HKG', 'Hong Kong');
+insert into COUNTRIES (code, name)
+values ('HUN', 'Hungary');
+insert into COUNTRIES (code, name)
+values ('ISL', 'Iceland');
+insert into COUNTRIES (code, name)
+values ('IOA', 'Independent Olympic Athletes');
+insert into COUNTRIES (code, name)
+values ('IOP', 'Independent Olympic Participants');
+insert into COUNTRIES (code, name)
+values ('IPP', 'Independent Paralympic Participants');
+insert into COUNTRIES (code, name)
+values ('IND', 'India');
+commit;
+prompt 100 records committed...
+insert into COUNTRIES (code, name)
+values ('IPA', 'Individual Paralympic Athletes');
+insert into COUNTRIES (code, name)
+values ('INA', 'Indonesia');
+insert into COUNTRIES (code, name)
+values ('IRI', 'Iran');
+insert into COUNTRIES (code, name)
+values ('IRQ', 'Iraq');
+insert into COUNTRIES (code, name)
+values ('IRL', 'Ireland');
+insert into COUNTRIES (code, name)
+values ('ISR', 'Israel');
+insert into COUNTRIES (code, name)
+values ('ITA', 'Italy');
+insert into COUNTRIES (code, name)
+values ('CIV', 'Ivory Coast');
+insert into COUNTRIES (code, name)
+values ('JAM', 'Jamaica');
+insert into COUNTRIES (code, name)
+values ('JPN', 'Japan');
+insert into COUNTRIES (code, name)
+values ('JOR', 'Jordan');
+insert into COUNTRIES (code, name)
+values ('KAZ', 'Kazakhstan');
+insert into COUNTRIES (code, name)
+values ('KEN', 'Kenya');
+insert into COUNTRIES (code, name)
+values ('KHM', 'Khmer Republic');
+insert into COUNTRIES (code, name)
+values ('KIR', 'Kiribati');
+insert into COUNTRIES (code, name)
+values ('COR', 'Korea');
+insert into COUNTRIES (code, name)
+values ('KOS', 'Kosovo');
+insert into COUNTRIES (code, name)
+values ('KUW', 'Kuwait');
+insert into COUNTRIES (code, name)
+values ('KGZ', 'Kyrgyzstan');
+insert into COUNTRIES (code, name)
+values ('LAO', 'Laos');
+insert into COUNTRIES (code, name)
+values ('LAT', 'Latvia');
+insert into COUNTRIES (code, name)
+values ('LBN', 'Lebanon');
+insert into COUNTRIES (code, name)
+values ('LES', 'Lesotho');
+insert into COUNTRIES (code, name)
+values ('LBR', 'Liberia');
+insert into COUNTRIES (code, name)
+values ('LBA', 'Libya');
+insert into COUNTRIES (code, name)
+values ('LIE', 'Liechtenstein');
+insert into COUNTRIES (code, name)
+values ('LTU', 'Lithuania');
+insert into COUNTRIES (code, name)
+values ('LUX', 'Luxembourg');
+insert into COUNTRIES (code, name)
+values ('MAC', 'Macau');
+insert into COUNTRIES (code, name)
+values ('MAD', 'Madagascar');
+insert into COUNTRIES (code, name)
+values ('MAW', 'Malawi');
+insert into COUNTRIES (code, name)
+values ('MAL', 'Malaya');
+insert into COUNTRIES (code, name)
+values ('MAS', 'Malaysia');
+insert into COUNTRIES (code, name)
+values ('MDV', 'Maldives');
+insert into COUNTRIES (code, name)
+values ('MLI', 'Mali');
+insert into COUNTRIES (code, name)
+values ('MLT', 'Malta');
+insert into COUNTRIES (code, name)
+values ('MHL', 'Marshall Islands');
+insert into COUNTRIES (code, name)
+values ('MTN', 'Mauritania');
+insert into COUNTRIES (code, name)
+values ('MRI', 'Mauritius');
+insert into COUNTRIES (code, name)
+values ('MEX', 'Mexico');
+insert into COUNTRIES (code, name)
+values ('ZZX', 'Mixed team');
+insert into COUNTRIES (code, name)
+values ('MIX', 'Mixed-NOCs');
+insert into COUNTRIES (code, name)
+values ('MDA', 'Moldova');
+insert into COUNTRIES (code, name)
+values ('MON', 'Monaco');
+insert into COUNTRIES (code, name)
+values ('MGL', 'Mongolia');
+insert into COUNTRIES (code, name)
+values ('MNE', 'Montenegro');
+insert into COUNTRIES (code, name)
+values ('MAR', 'Morocco');
+insert into COUNTRIES (code, name)
+values ('MOZ', 'Mozambique');
+insert into COUNTRIES (code, name)
+values ('MYA', 'Myanmar');
+insert into COUNTRIES (code, name)
+values ('NAM', 'Namibia');
+insert into COUNTRIES (code, name)
+values ('NRU', 'Nauru');
+insert into COUNTRIES (code, name)
+values ('NEP', 'Nepal');
+insert into COUNTRIES (code, name)
+values ('NED', 'Netherlands');
+insert into COUNTRIES (code, name)
+values ('AHO', 'Netherlands Antilles');
+insert into COUNTRIES (code, name)
+values ('NPA', 'Neutral Paralympic Athletes');
+insert into COUNTRIES (code, name)
+values ('NZL', 'New Zealand');
+insert into COUNTRIES (code, name)
+values ('NCA', 'Nicaragua');
+insert into COUNTRIES (code, name)
+values ('NIG', 'Niger');
+insert into COUNTRIES (code, name)
+values ('NGR', 'Nigeria');
+insert into COUNTRIES (code, name)
+values ('NBO', 'North Borneo');
+insert into COUNTRIES (code, name)
+values ('PRK', 'North Korea');
+insert into COUNTRIES (code, name)
+values ('MKD', 'North Macedonia');
+insert into COUNTRIES (code, name)
+values ('YAR', 'North Yemen');
+insert into COUNTRIES (code, name)
+values ('NRH', 'Northern Rhodesia');
+insert into COUNTRIES (code, name)
+values ('NOR', 'Norway');
+insert into COUNTRIES (code, name)
+values ('OAR', 'Olympic Athletes from Russia');
+insert into COUNTRIES (code, name)
+values ('OMA', 'Oman');
+insert into COUNTRIES (code, name)
+values ('PAK', 'Pakistan');
+insert into COUNTRIES (code, name)
+values ('PLW', 'Palau');
+insert into COUNTRIES (code, name)
+values ('PLE', 'Palestine');
+insert into COUNTRIES (code, name)
+values ('PAN', 'Panama');
+insert into COUNTRIES (code, name)
+values ('PNG', 'Papua New Guinea');
+insert into COUNTRIES (code, name)
+values ('PAR', 'Paraguay');
+insert into COUNTRIES (code, name)
+values ('PER', 'Peru');
+insert into COUNTRIES (code, name)
+values ('PHI', 'Philippines');
+insert into COUNTRIES (code, name)
+values ('POL', 'Poland');
+insert into COUNTRIES (code, name)
+values ('POR', 'Portugal');
+insert into COUNTRIES (code, name)
+values ('PUR', 'Puerto Rico');
+insert into COUNTRIES (code, name)
+values ('QAT', 'Qatar');
+insert into COUNTRIES (code, name)
+values ('ROT', 'Refugee Olympic Team');
+insert into COUNTRIES (code, name)
+values ('ROC', 'Republic of China');
+insert into COUNTRIES (code, name)
+values ('CGO', 'Republic of the Congo');
+insert into COUNTRIES (code, name)
+values ('RHO', 'Rhodesia');
+insert into COUNTRIES (code, name)
+values ('ROU', 'Romania');
+insert into COUNTRIES (code, name)
+values ('RUS', 'Russia');
+insert into COUNTRIES (code, name)
+values ('RU1', 'Russian Empire');
+insert into COUNTRIES (code, name)
+values ('RWA', 'Rwanda');
+insert into COUNTRIES (code, name)
+values ('SAA', 'Saar');
+insert into COUNTRIES (code, name)
+values ('SKN', 'Saint Kitts and Nevis');
+insert into COUNTRIES (code, name)
+values ('LCA', 'Saint Lucia');
+insert into COUNTRIES (code, name)
+values ('VIN', 'Saint Vincent and the Grenadines');
+insert into COUNTRIES (code, name)
+values ('SAM', 'Samoa');
+insert into COUNTRIES (code, name)
+values ('SMR', 'San Marino');
+insert into COUNTRIES (code, name)
+values ('KSA', 'Saudi Arabia');
+insert into COUNTRIES (code, name)
+values ('SEN', 'Senegal');
+insert into COUNTRIES (code, name)
+values ('SRB', 'Serbia');
+insert into COUNTRIES (code, name)
+values ('SCG', 'Serbia and Montenegro');
+insert into COUNTRIES (code, name)
+values ('SEY', 'Seychelles');
+insert into COUNTRIES (code, name)
+values ('SLE', 'Sierra Leone');
+insert into COUNTRIES (code, name)
+values ('SGP', 'Singapore');
+commit;
+prompt 200 records committed...
+insert into COUNTRIES (code, name)
+values ('SVK', 'Slovakia');
+insert into COUNTRIES (code, name)
+values ('SLO', 'Slovenia');
+insert into COUNTRIES (code, name)
+values ('SOL', 'Solomon Islands');
+insert into COUNTRIES (code, name)
+values ('SOM', 'Somalia');
+insert into COUNTRIES (code, name)
+values ('RSA', 'South Africa');
+insert into COUNTRIES (code, name)
+values ('KOR', 'South Korea');
+insert into COUNTRIES (code, name)
+values ('SSD', 'South Sudan');
+insert into COUNTRIES (code, name)
+values ('YMD', 'South Yemen');
+insert into COUNTRIES (code, name)
+values ('URS', 'Soviet Union');
+insert into COUNTRIES (code, name)
+values ('ESP', 'Spain');
+insert into COUNTRIES (code, name)
+values ('SRI', 'Sri Lanka');
+insert into COUNTRIES (code, name)
+values ('SUD', 'Sudan');
+insert into COUNTRIES (code, name)
+values ('SUR', 'Suriname');
+insert into COUNTRIES (code, name)
+values ('SWZ', 'Swaziland');
+insert into COUNTRIES (code, name)
+values ('SWE', 'Sweden');
+insert into COUNTRIES (code, name)
+values ('SUI', 'Switzerland');
+insert into COUNTRIES (code, name)
+values ('SYR', 'Syria');
+insert into COUNTRIES (code, name)
+values ('STP', 'São Tomé and Príncipe');
+insert into COUNTRIES (code, name)
+values ('TJK', 'Tajikistan');
+insert into COUNTRIES (code, name)
+values ('TAN', 'Tanzania');
+insert into COUNTRIES (code, name)
+values ('THA', 'Thailand');
+insert into COUNTRIES (code, name)
+values ('GAM', 'The Gambia');
+insert into COUNTRIES (code, name)
+values ('TOG', 'Togo');
+insert into COUNTRIES (code, name)
+values ('TGA', 'Tonga');
+insert into COUNTRIES (code, name)
+values ('TTO', 'Trinidad and Tobago');
+insert into COUNTRIES (code, name)
+values ('TUN', 'Tunisia');
+insert into COUNTRIES (code, name)
+values ('TUR', 'Turkey');
+insert into COUNTRIES (code, name)
+values ('TKM', 'Turkmenistan');
+insert into COUNTRIES (code, name)
+values ('TUV', 'Tuvalu');
+insert into COUNTRIES (code, name)
+values ('UGA', 'Uganda');
+insert into COUNTRIES (code, name)
+values ('UKR', 'Ukraine');
+insert into COUNTRIES (code, name)
+values ('EUN', 'Unified Team');
+insert into COUNTRIES (code, name)
+values ('UAE', 'United Arab Emirates');
+insert into COUNTRIES (code, name)
+values ('RAU', 'United Arab Republic');
+insert into COUNTRIES (code, name)
+values ('USA', 'United States');
+insert into COUNTRIES (code, name)
+values ('EUA', 'United Team of Germany');
+insert into COUNTRIES (code, name)
+values ('VOL', 'Upper Volta');
+insert into COUNTRIES (code, name)
+values ('URU', 'Uruguay');
+insert into COUNTRIES (code, name)
+values ('UZB', 'Uzbekistan');
+insert into COUNTRIES (code, name)
+values ('VAN', 'Vanuatu');
+insert into COUNTRIES (code, name)
+values ('VEN', 'Venezuela');
+insert into COUNTRIES (code, name)
+values ('VIE', 'Vietnam');
+insert into COUNTRIES (code, name)
+values ('ISV', 'Virgin Islands');
+insert into COUNTRIES (code, name)
+values ('FRG', 'West Germany');
+insert into COUNTRIES (code, name)
+values ('YEM', 'Yemen');
+insert into COUNTRIES (code, name)
+values ('YUG', 'Yugoslavia');
+insert into COUNTRIES (code, name)
+values ('ZAI', 'Zaire');
+insert into COUNTRIES (code, name)
+values ('ZAM', 'Zambia');
+insert into COUNTRIES (code, name)
+values ('ZIM', 'Zimbabwe');
+commit;
