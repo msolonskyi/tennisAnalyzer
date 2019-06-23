265d8528606c944813f4eec8f55f671f2387f7ad
msolonskyi <nick.solonsky@gmail.com>
2019-06-23 22:52:02 +0200

    update constants

--- python/loader/constants.py
+++ python/loader/constants.py
@@ -1,6 +1,20 @@
 CONNECTION_STRING = '/@'
 CHUNK_SIZE = 100
 BORDER_QTY = 5 # minimun matches per year per player for reload player
-URL_PREFIX = "http://www.atpworldtour.com"
+URL_PREFIX = 'http://www.atpworldtour.com'
 TOURNAMENT_TYPES = ('gs', '1000', 'atp', 'ch')
 DURATION_IN_DAYS = 17
+
+ROUND_NAMES_MAP = {
+    'F': 'Finals',
+    'SF': 'Semi-Finals',
+    'QF': 'Quarter-Finals',
+    'R16': 'Round of 16',
+    'R32': 'Round of 32',
+    'R64': 'Round of 64',
+    'R128': 'Round of 128',
+    'RR': 'Round Robin',
+    'Q1': '1st Round Qualifying',
+    'Q2': '2nd Round Qualifying',
+    'Q3': '3rd Round Qualifying',
+    'BR': 'Olympic Bronze'}
