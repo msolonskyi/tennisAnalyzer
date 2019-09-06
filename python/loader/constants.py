fd44d2226e1237069cdd76e857e28b3baee2945c
msolonskyi <nick.solonsky@gmail.com>
2019-09-06 22:42:56 +0200

    extract to CSV has been added

--- python/loader/constants.py
+++ python/loader/constants.py
@@ -4,6 +4,7 @@ BORDER_QTY = 5 # minimun matches per year per player for reload player
 URL_PREFIX = 'http://www.atpworldtour.com'
 TOURNAMENT_TYPES = ('gs', '1000', 'atp', 'ch')
 DURATION_IN_DAYS = 17
+CSV_PATH = ''
 
 ROUND_NAMES_MAP = {
     'F': 'Finals',
