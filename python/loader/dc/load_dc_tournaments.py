286ae6834cfab5931d260ac7485665a4342e3861
msolonskyi <nick.solonsky@gmail.com>
2019-09-21 11:42:40 +0200

    Split loaders to ATP and DC

--- python/loader/dc/load_dc_tournaments.py
+++ python/loader/dc/load_dc_tournaments.py
@@ -79,6 +79,7 @@ try:
     cur = con.cursor()
     #
     for country_code in ['ALB', 'ALG', 'AND', 'ANG', 'ANT', 'ARG', 'ARM', 'ARU', 'AUS', 'AUT', 'AZE', 'BAH', 'BAN', 'BAR', 'BEL', 'BEN', 'BER', 'BIH', 'BLR', 'BOL', 'BOT', 'BRA', 'BRN', 'BRU', 'BUL', 'BUR', 'CAM', 'CAN', 'CAR', 'CGO', 'CHI', 'CHN', 'CIV', 'CMR', 'COL', 'CRC', 'CRO', 'CUB', 'CYP', 'CZE', 'DEN', 'DJI', 'DOM', 'ECA', 'ECU', 'EGY', 'ESA', 'ESP', 'EST', 'ETH', 'FIJ', 'FIN', 'FRA', 'GAB', 'GBR', 'GEO', 'GER', 'GHA', 'GRE', 'GUA', 'GUM', 'HAI', 'HKG', 'HON', 'HUN', 'INA', 'IND', 'IRI', 'IRL', 'IRQ', 'ISL', 'ISR', 'ISV', 'ITA', 'JAM', 'JOR', 'JPN', 'KAZ', 'KEN', 'KGZ', 'KOR', 'KOS', 'KSA', 'KUW', 'LAT', 'LBA', 'LBN', 'LCA', 'LES', 'LIE', 'LTU', 'LUX', 'MAD', 'MAR', 'MAS', 'MDA', 'MEX', 'MGL', 'MKD', 'MLI', 'MLT', 'MNE', 'MON', 'MOZ', 'MRI', 'MYA', 'NAM', 'NED', 'NGR', 'NOR', 'NZL', 'OMA', 'PAK', 'PAN', 'PAR', 'PER', 'PHI', 'POC', 'POL', 'POR', 'PUR', 'QAT', 'ROU', 'RSA', 'RUS', 'RWA', 'SEN', 'SGP', 'SLO', 'SMR', 'SRB', 'SRI', 'SUD', 'SUI', 'SVK', 'SWE', 'SYR', 'THA', 'TJK', 'TKM', 'TOG', 'TPE', 'TTO', 'TUN', 'TUR', 'UAE', 'UGA', 'UKR', 'URU', 'USA', 'UZB', 'VEN', 'VIE', 'YEM', 'ZAM', 'ZIM']:
+        logzero.logger.info(country_code)
         data = parse(country_code)
         cur.execute('truncate table stg_tournaments')
         # insert
