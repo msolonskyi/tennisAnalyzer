22bafd962ef2fb305b1a445dc5ff35739fed4c9e
msolonskyi <nick.solonsky@gmail.com>
2023-01-15 20:44:34 -0500

    update delta hash sp have been added

---
+++ db/schemas/tennisAnalyzer/plsql/sp_update_teams_dc_delta_hash.sql
@@ -0,0 +1,13 @@
+﻿create or replace procedure sp_update_teams_dc_delta_hash(
+  pn_country_code teams_dc.country_code%type)
+is
+begin
+  update teams_dc t
+    set
+      delta_hash = sf_teams_dc_delta_hash(
+             pn_country_code => pn_country_code,
+             pn_name         => t.name,
+             pn_url          => t.url)
+  where t.country_code = pn_country_code;
+end sp_update_teams_dc_delta_hash;
+/
