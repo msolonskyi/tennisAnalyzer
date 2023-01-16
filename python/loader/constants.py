80d90c112b26eea0973d7076c29774bd143d37a7
msolonskyi <nick.solonsky@gmail.com>
2023-01-15 20:46:15 -0500

    ITF support has been added

--- python/loader/constants.py
+++ python/loader/constants.py
@@ -3,12 +3,29 @@ CHUNK_SIZE = 100
 BORDER_QTY = 5  # minimun matches per year per player for reload player
 ATP_URL_PREFIX = 'http://www.atpworldtour.com'
 DC_URL_PREFIX = 'https://www.daviscup.com'
+ITF_URL_PREFIX = 'https://www.itftennis.com'
 ATP_TOURNAMENT_SERIES = ('gs', '1000', 'atp', 'ch')
+ITF_TOURNAMENT_SERIES = ('fu')
 DC_TOURNAMENT_SERIES = ('dc',)
-DURATION_IN_DAYS = 18
+DURATION_IN_DAYS = 22
 ATP_CSV_PATH = ''
 DC_CSV_PATH = ''
 SLEEP_DURATION = 10
+
+MONTHS_MAP = {
+    'Jan' : '01',
+    'Feb' : '02',
+    'Mar' : '03',
+    'Apr' : '04',
+    'May' : '05',
+    'Jun' : '06',
+    'Jul' : '07',
+    'Aug' : '08',
+    'Sep' : '09',
+    'Oct' : '10',
+    'Nov' : '11',
+    'Dec' : '12'
+}
 
 COUNTRY_CODE_MAP = {
     'LIB': 'LBN',
