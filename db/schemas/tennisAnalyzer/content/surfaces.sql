3bcd21d37254be9b2c4ae4f08c071492bb11afd5
msolonskyi <nick.solonsky@gmail.com>
2023-02-05 00:33:21 -0500

    surfaces table has been added

---
+++ db/schemas/tennisAnalyzer/content/surfaces.sql
@@ -0,0 +1,16 @@
+﻿set feedback off
+set define off
+prompt Loading SURFACES...
+insert into SURFACES (surface)
+values ('Carpet');
+insert into SURFACES (surface)
+values ('Clay');
+insert into SURFACES (surface)
+values ('Grass');
+insert into SURFACES (surface)
+values ('Hard');
+commit;
+prompt 4 records loaded
+set feedback on
+set define on
+prompt Done.
