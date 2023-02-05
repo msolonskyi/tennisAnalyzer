6627316ed28739674024be005d44d828f280691c
msolonskyi <nick.solonsky@gmail.com>
2023-02-05 00:32:51 -0500

    indoor_outdoor table has been added

---
+++ db/schemas/tennisAnalyzer/content/indoor_outdoor.sql
@@ -0,0 +1,12 @@
+﻿set feedback off
+set define off
+prompt Loading INDOOR_OUTDOOR...
+insert into INDOOR_OUTDOOR (indoor_outdoor)
+values ('Indoor');
+insert into INDOOR_OUTDOOR (indoor_outdoor)
+values ('Outdoor');
+commit;
+prompt 2 records loaded
+set feedback on
+set define on
+prompt Done.
