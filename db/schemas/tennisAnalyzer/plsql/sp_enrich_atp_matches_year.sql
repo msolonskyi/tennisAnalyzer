68516fc7c6675659fde8e3e913bcfd3c13ea12f3
msolonskyi <nick.solonsky@gmail.com>
2023-05-28 00:36:32 -0400

    enrichment by year has been added

---
+++ db/schemas/tennisAnalyzer/plsql/sp_enrich_atp_matches_year.sql
@@ -0,0 +1,14 @@
+﻿create or replace procedure sp_enrich_atp_matches_year(p_year atp_tournaments.year%type)
+is
+  v_match_ids t_list_of_varchar := t_list_of_varchar();
+begin
+  for rec in (select m.id from atp_matches m, atp_tournaments t where m.tournament_id = t.id and t.year = p_year)
+  loop
+      v_match_ids.extend;
+      v_match_ids(v_match_ids.count) := rec.id;
+  end loop;
+  --
+  sp_enrich_atp_matches(v_match_ids);
+  --
+end sp_enrich_atp_matches_year;
+/
