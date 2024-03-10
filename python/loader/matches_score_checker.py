201a9d3dd6ad7ac38eb9c9c6cc704f92ba1d5e91
msolonskyi <nick.solonsky@gmail.com>
2024-03-10 10:01:59 -0400

    Score checker has been fixed

--- python/loader/matches_score_checker.py
+++ python/loader/matches_score_checker.py
@@ -12,6 +12,7 @@ class MatchesScoreChecker(MatchesBaseLoader):
     def _init(self):
         self.LOGFILE_NAME = os.path.splitext(os.path.basename(__file__))[0] + '.log'
         super()._init()
+        logzero.logfile(self.LOGFILE_NAME, loglevel=logzero.logging.WARNING)
 
     def _parse(self):
         self._fill_matches_list()
@@ -22,9 +23,10 @@ class MatchesScoreChecker(MatchesBaseLoader):
     def _fill_matches_list(self):
         try:
             cur = self.con.cursor()
-            sql = '''select id, tournament_code, match_score
+            sql = '''select id, tournament_code, score
 from vw_matches
 where tournament_year = :year
+  and series_id != 'dc'
 order by tournament_start_dtm, tournament_code, stadie_ord'''
             self._matches_list = cur.execute(sql, {'year': self.year}).fetchall()
             logzero.logger.info(f'checking {cur.rowcount} matches for {self.year} year')
