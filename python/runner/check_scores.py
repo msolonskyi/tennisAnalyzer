84883609a570577f3ce8e51963f8754ce0ded190
msolonskyi <mykola.solonskyi@gmail.com>
2024-12-08 21:31:58 -0500

    Loading 1 year only

--- python/runner/check_scores.py
+++ python/runner/check_scores.py
@@ -1,11 +1,17 @@
 from matches_score_checker import MatchesScoreChecker
 from datetime import datetime
+import sys
 
 
 def main():
-    for year in range(1999, datetime.today().year + 1):
+    if len(sys.argv) >= 2:
+        year = sys.argv[1]
         score_checker = MatchesScoreChecker(year)
         score_checker.load()
+    else:
+        for year in range(1999, datetime.today().year + 1):
+            score_checker = MatchesScoreChecker(year)
+            score_checker.load()
 
 if __name__ == "__main__":
     main()
