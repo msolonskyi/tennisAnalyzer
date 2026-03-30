b91ae87b5a0b79e72eaecf667949335bad5bb3f3
msolonskyi <mykola.solonskyi@gmail.com>
2026-03-29 22:37:45 -0400

    Deleting CSV files older 30 days.

--- bash/9.restart/run.sh
+++ bash/9.restart/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+find /home/opc/projects/.tennis/csv  -type f -mtime +30 -delete
+
+rm -f /home/opc/projects/.tennis/0.0.load_atp_all/go.run
+rm -f /home/opc/projects/.tennis/0.0.load_tournaments/go.run
 sudo shutdown -r now >> log.log
